af6fde56a8bad2f5108353353af34ddd2d5f3c10
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2024-11-01 14:51:21 +0200

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,86 @@
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+
+import "@solarity/hardhat-migrate";
+
+import "@typechain/hardhat";
+
+import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
+
+import "solidity-coverage";
+
+import "tsconfig-paths/register";
+
+import { HardhatUserConfig } from "hardhat/config";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+function privateKey() {
+  return process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
+}
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      initialDate: "1970-01-01T00:00:00Z",
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      gasMultiplier: 1.2,
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: privateKey(),
+      gasMultiplier: 1.2,
+    },
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: privateKey(),
+      gasMultiplier: 1.2,
+    },
+  },
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "paris",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: `${process.env.ETHERSCAN_KEY}`,
+      mainnet: `${process.env.ETHERSCAN_KEY}`,
+    },
+  },
+  migrate: {
+    pathToMigrations: "./deploy/",
+  },
+  mocha: {
+    timeout: 1000000,
+  },
+  contractSizer: {
+    alphaSort: false,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 50,
+    enabled: false,
+    coinmarketcap: `${process.env.COINMARKETCAP_KEY}`,
+  },
+  typechain: {
+    outDir: "generated-types/ethers",
+    target: "ethers-v6",
+    alwaysGenerateOverloads: true,
+    discriminateTypes: true,
+  },
+};
+
+export default config;
